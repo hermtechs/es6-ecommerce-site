72b9cc1d04b0710ed1f03afd783ec54a5452227a
hermtechs <hermtechs@gmail.com>
2021-07-03 19:06:36 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//variables
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+
+//cart
+let cart = [];
+
+//gettiing products
+class Products{
+
+}
+
+//display shop products
+class UI{
+
+}
+
+//local storage
+class Storage{
+    
+}
